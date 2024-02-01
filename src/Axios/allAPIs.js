c1cac594a9959eca95e856d8d2409222c8313193
Arshil <arshilamaanansari@gmail.com>
2024-02-01 21:13:21 +0530

    updated base URL for backend

--- src/Axios/allAPIs.js
+++ src/Axios/allAPIs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL:  "http://localhost:5000"
-    baseURL:  "https://vast-jade-panda-cape.cyclic.app"
+    baseURL:  "https://learn-lite-backend.onrender.com"
 });
 
 export default axiosInstance;
